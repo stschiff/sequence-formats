b90cd7a6c9b461be863168a5b008663927c6f97b
Stephan Schiffels <schiffels_schiffels@mac.com>
2018-02-15 20:36:23 +0100

    changed all String to Text

--- src/SequenceFormats/FreqSum.hs
+++ src/SequenceFormats/FreqSum.hs
@@ -9,27 +9,28 @@ import Control.Monad.Catch (MonadThrow, throwM)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.State.Strict (runStateT)
 import qualified Data.Attoparsec.Text as A
-import Data.Char (isAlphaNum)
-import Data.List (intercalate)
-import Data.Text (unpack, Text)
+import Data.Char (isAlphaNum, isSpace)
+import Data.Text (unpack, Text, intercalate, singleton)
 import Pipes (Producer, runEffect, (>->))
 import Pipes.Attoparsec (parse, ParsingError(..))
 import qualified Pipes.Prelude as P
 import Pipes.Safe (MonadSafe)
 import qualified Pipes.Text.IO as PT
+import Turtle (format, s, d, (%))
 
 data FreqSumHeader = FreqSumHeader {
-    fshNames :: [String],
+    fshNames :: [Text],
     fshCounts :: [Int]
 } deriving (Eq)
 
 instance Show FreqSumHeader where
-    show (FreqSumHeader names nCounts) = "#CHROM\tPOS\tREF\tALT\t" ++ intercalate "\t" tuples
+    show (FreqSumHeader names nCounts) =
+        unpack $ format ("#CHROM\tPOS\tREF\tALT\t"%s) (intercalate "\t" tuples)
       where
-        tuples = zipWith (\n c -> n ++ "(" ++ show c ++ ")") names nCounts
+        tuples = zipWith (\n c -> format (s%"("%d%")") n c) names nCounts
 
 data FreqSumEntry = FreqSumEntry {
-    fsChrom  :: Int,
+    fsChrom  :: Text,
     fsPos    :: Int,
     fsRef    :: Char,
     fsAlt    :: Char,
@@ -38,7 +39,10 @@ data FreqSumEntry = FreqSumEntry {
 
 instance Show FreqSumEntry where
     show (FreqSumEntry chrom pos ref alt counts) =
-        intercalate "\t" [show chrom, show pos, [ref], [alt], intercalate "\t" . map show $ counts]
+        unpack $ format (s%"\t"%d%"\t"%s%"\t"%s%"\t"%s) chrom pos (singleton ref) (singleton alt) 
+            countStr 
+      where
+        countStr = intercalate "\t" (map (format d) counts)
 
 readFreqSumProd :: (MonadThrow m) =>
     Producer Text m () -> m (FreqSumHeader, Producer FreqSumEntry m ())
@@ -59,16 +63,15 @@ readFreqSumFile = readFreqSumProd . PT.readFile
 parseFreqSumHeader :: A.Parser FreqSumHeader
 parseFreqSumHeader = do
     tuples <- A.string "#CHROM\tPOS\tREF\tALT\t" >> A.sepBy' tuple A.space <* A.endOfLine
-    let names = map (unpack . fst) tuples
+    let names = map fst tuples
         counts = map snd tuples
     return $ FreqSumHeader names counts
   where
     tuple = (,) <$> A.takeWhile (\c -> isAlphaNum c || c == '_' || c == '-') <* A.char '(' <*> A.decimal <* A.char ')'
 
 parseFreqSumEntry :: A.Parser FreqSumEntry
-parseFreqSumEntry = FreqSumEntry <$> A.decimal <* A.skipSpace <*> A.decimal <* A.skipSpace <*>
-                                     A.letter <* A.skipSpace <*> A.letter <* A.skipSpace <*>
-                                     counts <* A.endOfLine
+parseFreqSumEntry = FreqSumEntry <$> A.takeTill isSpace <* A.skipSpace <*> A.decimal <*
+    A.skipSpace <*> A.letter <* A.skipSpace <*> A.letter <* A.skipSpace <*> counts <* A.endOfLine
   where
     counts = (A.signed A.decimal) `A.sepBy` A.char '\t'
 
