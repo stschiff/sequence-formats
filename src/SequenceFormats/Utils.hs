721a61a188f52fd7eb6acd5197223e4938dba467
Stephan Schiffels <schiffels_schiffels@mac.com>
2018-03-23 13:25:00 +0100

    improved error message in liftParsingErrors

--- src/SequenceFormats/Utils.hs
+++ src/SequenceFormats/Utils.hs
@@ -3,9 +3,10 @@ module SequenceFormats.Utils (liftParsingErrors, consumeProducer, FormatExceptio
 
 import Control.Exception (Exception)
 import Control.Monad.Catch (MonadThrow, throwM)
+import Control.Monad.Trans.Class (lift)
 import Data.Text (Text)
 import qualified Data.Attoparsec.Text as A
-import Pipes (Producer)
+import Pipes (Producer, next)
 import Pipes.Attoparsec (ParsingError(..), parsed)
 
 data FormatException = FormatException Text
@@ -16,7 +17,10 @@ instance Exception FormatException
 liftParsingErrors :: (MonadThrow m) =>
     Either (ParsingError, Producer Text m r) () -> Producer a m ()
 liftParsingErrors res = case res of
-    Left (e, _) -> throwM e
+    Left (ParsingError cont msg, restProd) -> do
+        Right (chunk, _) <- lift $ next restProd
+        let msg' = msg ++ " Error occurred while trying to parse this chunk: " ++ show chunk
+        throwM (ParsingError cont msg')
     Right () -> return ()
 
 consumeProducer :: (MonadThrow m) => A.Parser a -> Producer Text m () -> Producer a m ()
