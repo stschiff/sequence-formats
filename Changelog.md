ea9316cecd8b5b708e70fc99b0893d8c5487142e
Stephan Schiffels <schiffels_schiffels@mac.com>
2019-03-12 22:43:16 +0100

    added new functions to output Eigenstrat and Bim files

--- Changelog.md
+++ Changelog.md
@@ -7,3 +7,5 @@ V 1.1.5: Fixed VCF parser: Now breaks if lines end prematurely
 V 1.1.6: VCF parser now allows for truncated VCF files with no Format and Genotypes (sites-only VCF files)
 
 V 1.1.7: Added option to parse Bim file (slightly different layout to Eigenstrat Snp Format), and added genetic position and snpId to the EigenstratSnpEntry datastructure. This will cause breaking changes in code linking against this library.
+
+V 1.1.8: Added more consumers for Bim and Eigenstrat Snp files
--- src/SequenceFormats/Eigenstrat.hs
+++ src/SequenceFormats/Eigenstrat.hs
@@ -7,7 +7,8 @@
 module SequenceFormats.Eigenstrat (EigenstratSnpEntry(..), EigenstratIndEntry(..), 
     readEigenstratInd, GenoEntry(..), GenoLine, Sex(..), 
     readEigenstratSnpStdIn, readEigenstratSnpFile, readBimStdIn, readBimFile,
-    readEigenstrat, writeEigenstrat) where
+    readEigenstrat, writeEigenstrat, writeEigenstratIndFile, writeEigenstratSnp, writeBim, 
+    writeEigenstratGeno) where
 
 import SequenceFormats.Utils (consumeProducer, FormatException(..), Chrom(..))
 
@@ -22,9 +23,10 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Pipes (Producer, Pipe, (>->), for, cat, yield, Consumer)
 import Pipes.Safe (MonadSafe)
+import qualified Pipes.Safe.Prelude as PS
 import qualified Pipes.Prelude as P
 import qualified Pipes.Text.IO as PT
-import System.IO (withFile, IOMode(..))
+import System.IO (withFile, IOMode(..), Handle)
 import Turtle (format, w, d, (%), s, g)
 
 -- |A datatype to represent a single genomic SNP. The constructor arguments are:
@@ -53,25 +55,25 @@ type GenoLine = Vector GenoEntry
 
 eigenstratSnpParser :: A.Parser EigenstratSnpEntry
 eigenstratSnpParser = do
-    snpId <- A.skipMany A.space >> word
+    snpId_ <- A.skipMany A.space >> word
     chrom <- A.skipMany1 A.space >> word
     geneticPos <- A.skipMany1 A.space >> A.double
     pos <- A.skipMany1 A.space >> A.decimal
     ref <- A.skipMany1 A.space >> A.satisfy (A.inClass "ACTGN")
     alt <- A.skipMany1 A.space >> A.satisfy (A.inClass "ACTGX")
     void A.endOfLine
-    return $ EigenstratSnpEntry (Chrom chrom) pos geneticPos snpId ref alt
+    return $ EigenstratSnpEntry (Chrom chrom) pos geneticPos snpId_ ref alt
 
 bimParser :: A.Parser EigenstratSnpEntry
 bimParser = do
     chrom <- word
-    snpId <- A.skipMany1 A.space >> word
+    snpId_ <- A.skipMany1 A.space >> word
     geneticPos <- A.skipMany1 A.space >> A.double
     pos <- A.skipMany1 A.space >> A.decimal
     ref <- A.skipMany1 A.space >> A.satisfy (A.inClass "ACTGN")
     alt <- A.skipMany1 A.space >> A.satisfy (A.inClass "ACTGX")
     void A.endOfLine
-    return $ EigenstratSnpEntry (Chrom chrom) pos geneticPos snpId ref alt
+    return $ EigenstratSnpEntry (Chrom chrom) pos geneticPos snpId_ ref alt
     
 word :: A.Parser T.Text
 word = A.takeTill isSpace
@@ -153,29 +155,7 @@ validateEigenstratEntries nr = for cat $ \line -> do
     else
         yield line
 
--- |Function to write an Eigenstrat Database. Returns a consumer expecting joint Snp- and Genotype lines.
-writeEigenstrat :: (MonadSafe m) => FilePath -- ^The Genotype file
-                -> FilePath -- ^The Snp File
-                -> FilePath -- ^The Ind file
-                -> [EigenstratIndEntry] -- ^The list of individual entries
-                -> Consumer (EigenstratSnpEntry, GenoLine) m () -- ^A consumer to read joint Snp/Genotype entries.
-writeEigenstrat genoFile snpFile indFile indEntries = do
-    liftIO $ writeEigenstratIndFile indFile indEntries
-    let snpOutTextConsumer = PT.writeFile snpFile
-        genoOutTextConsumer = PT.writeFile genoFile
-        toTextPipe = P.map (\(EigenstratSnpEntry chrom pos gpos gid ref alt, genoLine) ->
-            let snpLine = format (s%"\t"%s%"\t"%g%"\t"%d%"\t"%s%"\t"%s%"\n") gid (unChrom chrom) 
-                    gpos pos (T.singleton ref) (T.singleton alt)
-                genoLineStr = T.concat . map (format d . toEigenStratNum) . toList $ genoLine
-            in  (snpLine, format (s%"\n") genoLineStr))
-    toTextPipe >-> P.tee (P.map fst >-> snpOutTextConsumer) >-> P.map snd >-> genoOutTextConsumer
-  where
-    toEigenStratNum c = case c of
-        HomRef -> 2 :: Int
-        Het -> 1
-        HomAlt -> 0
-        Missing -> 9
-
+-- |Function to write an Eigenstrat Ind file.
 writeEigenstratIndFile :: (MonadIO m) => FilePath -> [EigenstratIndEntry] -> m ()
 writeEigenstratIndFile f indEntries = do
     liftIO . withFile f WriteMode $ \h -> do
@@ -186,3 +166,52 @@ writeEigenstratIndFile f indEntries = do
         Male -> "M"
         Female -> "F"
         Unknown -> "U"
+
+-- |Function to write an Eigenstrat Snp File. Returns a consumer expecting EigenstratSnpEntries.
+writeEigenstratSnp :: (MonadIO m) => Handle -- ^The Eigenstrat Snp File Handle.
+    -> Consumer EigenstratSnpEntry m () -- ^A consumer to read EigenstratSnpEntries
+writeEigenstratSnp snpFileH =
+    let snpOutTextConsumer = PT.toHandle snpFileH
+        toTextPipe = P.map (\(EigenstratSnpEntry chrom pos gpos gid ref alt) ->
+            format (s%"\t"%s%"\t"%g%"\t"%d%"\t"%s%"\t"%s%"\n") gid (unChrom chrom) 
+                gpos pos (T.singleton ref) (T.singleton alt))
+    in  toTextPipe >-> snpOutTextConsumer
+
+-- |Function to write a Bim file. Returns a consumer expecting EigenstratSnpEntries.
+writeBim :: (MonadIO m) => Handle -- ^The Eigenstrat Snp File handle.
+    -> Consumer EigenstratSnpEntry m () -- ^A consumer to read EigenstratSnpEntries
+writeBim snpFileH =
+    let snpOutTextConsumer = PT.toHandle snpFileH
+        toTextPipe = P.map (\(EigenstratSnpEntry chrom pos gpos gid ref alt) ->
+            format (s%"\t"%s%"\t"%g%"\t"%d%"\t"%s%"\t"%s%"\n") (unChrom chrom) gid
+                gpos pos (T.singleton ref) (T.singleton alt))
+    in  toTextPipe >-> snpOutTextConsumer
+
+-- |Function to write an Eigentrat Geno File. Returns a consumer expecting Eigenstrat Genolines.
+writeEigenstratGeno :: (MonadIO m) => Handle -- ^The Genotype file handle
+                -> Consumer GenoLine m () -- ^A consumer to read Genotype entries.
+writeEigenstratGeno genoFileH =
+    let genoOutTextConsumer = PT.toHandle genoFileH
+        toTextPipe = P.map (\genoLine ->
+            let genoLineStr = T.concat . map (format d . toEigenStratNum) . toList $ genoLine
+            in  format (s%"\n") genoLineStr)
+    in  toTextPipe >-> genoOutTextConsumer
+  where
+    toEigenStratNum c = case c of
+        HomRef -> 2 :: Int
+        Het -> 1
+        HomAlt -> 0
+        Missing -> 9
+
+-- |Function to write an Eigenstrat Database. Returns a consumer expecting joint Snp- and Genotype lines.
+writeEigenstrat :: (MonadSafe m) => FilePath -- ^The Genotype file
+                -> FilePath -- ^The Snp File
+                -> FilePath -- ^The Ind file
+                -> [EigenstratIndEntry] -- ^The list of individual entries
+                -> Consumer (EigenstratSnpEntry, GenoLine) m () -- ^A consumer to read joint Snp/Genotype entries.
+writeEigenstrat genoFile snpFile indFile indEntries = do
+    liftIO $ writeEigenstratIndFile indFile indEntries
+    let snpOutConsumer = PS.withFile snpFile WriteMode writeEigenstratSnp
+        genoOutConsumer = PS.withFile genoFile WriteMode writeEigenstratGeno
+    P.tee (P.map fst >-> snpOutConsumer) >-> P.map snd >-> genoOutConsumer
+
